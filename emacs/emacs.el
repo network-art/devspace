546b917d1582a0742fcf6a190e136686290c683d
Guruprasad Nagaraj <gpn@networkart.com>
2019-06-29 15:15:05 +0530

    Added profiles for gnu autotools, emacs, java, and maven.

---
+++ emacs/emacs.el
@@ -0,0 +1,107 @@
+;; .emacs
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
+
+(add-to-list 'load-path "~/workspace/devspace/emacs/packages")
+
+;;; uncomment this line to disable loading of "default.el" at startup
+;; (setq inhibit-default-init t)
+
+;; enable visual feedback on selections
+;(setq transient-mark-mode t)
+
+;; default to better frame titles
+(setq frame-title-format
+      (concat  "%b - emacs@" (system-name)))
+
+;; default to unified diffs
+(setq diff-switches "-u")
+
+;; always end a file with a newline
+;(setq require-final-newline 'query)
+
+;;; uncomment for CJK utf-8 support for non-Asian users
+;; (require 'un-define)
+
+(require 'go-mode)
+(autoload 'go-mode "go-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+(setq-default indent-tabs-mode t)
+(setq-default tab-width 2)
+
+(setq c-basic-indent 2)
+(setq-default c-basic-offset 2
+              cperl-indent-level 2
+              js-indent-level 2
+              javascript-indent-level 2
+              web-mode-markup-indent-offset 2
+              web-mode-css-indent-offset 2
+              web-mode-code-indent-offset 2)
+(setq web-mode-attr-indent-offset 2)
+
+(add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+(add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+(add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+(add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
+
+(setq web-mode-enable-auto-indentation nil)
+
+;; Commented out in lieu of the above customization
+;; (setq-default indent-tabs-mode nil)
+;; (setq-default c-basic-offset 2)
+;; (setq-default cperl-indent-level 2)
+;; (setq web-mode-markup-indent-offset 2)
+;; (setq-default js-indent-level 2)
+;; (setq-default javascript-indent-level 2)
+;; (setq web-mode-css-indent-offset 2)
+;; (setq web-mode-code-indent-offset 2)
+
+(add-hook 'java-mode-hook (lambda ()
+                            (setq c-basic-offset 2
+                                  tab-width 2
+                                  indent-tabs-mode nil)))
+
+(setq nxml-child-indent 2)
+(add-hook 'nxml-mode-hook (lambda ()
+                            (setq tab-width 2
+                                  indent-tabs-mode nil)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(nxml-slash-auto-complete-flag t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "nil" :slant normal :weight light :height 120 :width normal)))))
+
+(put 'upcase-region 'disabled nil)
--- profile
+++ profile
@@ -1,4 +1,7 @@
 this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+. ${this_dir}/gnu_autotools/gnu_autotools_bash
 . ${this_dir}/go/go_bash
+. ${this_dir}/java/java_bash
+. ${this_dir}/maven/maven_bash
 . ${this_dir}/npm/npm_bash
